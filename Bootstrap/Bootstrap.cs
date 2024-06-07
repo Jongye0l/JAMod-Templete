277017214a41c156ef616a78067fc7f187517328
Jongyeol <45934107+Jongye0l@users.noreply.github.com>
2024-06-07 18:40:33 +0900

    Initial commit

---
+++ Bootstrap/Bootstrap.cs
@@ -0,0 +1,8 @@
+﻿using JALib.Bootstrap;
+using UnityModManagerNet;
+
+namespace JAMod_Templete {
+    public class Bootstrap {
+        public static void Setup(UnityModManager.ModEntry modEntry) => JABootstrap.Load(modEntry);
+    }
+}
